ff877348df83af7d53a907e050d02ecce8923f27
ijp85 <ijp85@X1_Extreme>
2023-09-03 11:51:04 +0900

    수정

--- HSC-HSP/src/main/java/com/ebs/ngs/hsc/hsp/web/api/datasync/controller/DataSyncController.java
+++ HSC-HSP/src/main/java/com/ebs/ngs/hsc/hsp/web/api/datasync/controller/DataSyncController.java
@@ -9,13 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-<<<<<<< HEAD
 import com.ebs.biz.api.common.response.BizDataSyncApiResponse;
 import com.ebs.biz.api.common.util.DateUtil;
-=======
-import com.ebs.biz.api.common.response.BizDataSyncApiResponse;
-import com.ebs.biz.api.common.util.DateUtil;
->>>>>>> branch 'main' of https://github.com/jpross/HSC-API.git
 import com.ebs.biz.api.common.validate.ApiKeyValidate;
 import com.ebs.ngs.hsc.hsp.web.api.datasync.service.DataSyncService;
 
@@ -34,20 +29,11 @@ public class DataSyncController {
 			@RequestParam(value = "apiKey", required = true) @Valid @ApiKeyValidate String apiKey,
 			@RequestParam(value = "date", required = false) String date
 			) {
-<<<<<<< HEAD
-		
-		if (date == null) {
-			date = DateUtil.getDate("yyyyMMdd", -1);
-			System.out.println("date: " + date);
-		}
-		
-=======
 		
 		if (date == null) {
 			date = DateUtil.getDate("yyyyMMdd", -1);
 		}
 		
->>>>>>> branch 'main' of https://github.com/jpross/HSC-API.git
 		BizDataSyncApiResponse<?> response = dataSyncService.getDatasByFactory(table, action, date);
 		
 		return response;
---
+++ HSC-HSP/src/main/java/com/ebs/ngs/hsc/hsp/web/api/datasync/service/DataSyncService.java
@@ -0,0 +1,78 @@
+package com.ebs.ngs.hsc.hsp.web.api.datasync.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ebs.biz.api.common.response.BizDataSyncApiResponse;
+import com.ebs.ngs.hsc.hsp.web.api.datasync.repository.DataSyncRepository;
+import com.ebs.ngs.hsc.hsp.web.common.mapper.DataBaseFactory.DataSource;
+
+@Service
+public class DataSyncService {
+	
+	private static final Logger logger = LoggerFactory.getLogger(DataSyncService.class);
+	
+	@Autowired
+	private DataSyncRepository repository;
+	
+	public <T> BizDataSyncApiResponse<T> getDatasByFactory(String tableCd, String action, String date) {
+		
+		List<T> datas = new ArrayList<>();
+		List<String> tables = TableSelector.getTable(tableCd);
+		
+		logger.info("==================================================================================");
+		logger.info("Biz DataSync API Service");
+		logger.info("==================================================================================");
+		logger.info("tableCd: " + tableCd);
+		logger.info("tables: " + String.join(",", tables));
+		logger.info("action: " + action);
+		logger.info("date: " + date);
+		
+		DataGetter getter = getDataGetter(action);
+		
+		tables.forEach(table -> {
+			datas.addAll(getData(getter, table, date));
+		});
+		
+		logger.info("data size: " + String.valueOf(datas.size()));
+		logger.info("==================================================================================");
+		
+		return new BizDataSyncApiResponse<T>()
+				.setDate(date)
+				.setData(datas);
+		
+	}
+	
+	public <T> List<T> getData(DataGetter getter, String table, String date) {
+		DataSource dataSource = (table.indexOf(".jhs") > -1) ? DataSource.JHS_SLAVE : DataSource.HSC_SLAVE;
+		if (table.indexOf(".hsc") > -1) {
+			dataSource = DataSource.HSC_SLAVE;
+		}
+		else {
+			dataSource = DataSource.JHS_SLAVE;
+		}
+		List<T> datas = getter.get(table, date, dataSource);
+		return datas;
+	}
+	
+	public DataGetter getDataGetter(String action) {
+		
+		switch (action) {
+			case "create":
+				return repository::getCreateData;
+			case "update":
+				return repository::getUpdateData;
+			case "delete":
+				return repository::getDeleteData;
+			default:
+				return null;
+		}
+		
+	}
+	
+}
